7724940d93891be349aea7bec138cd877f31c31a
Vlad Ciocoiu <vlad.014916@gmail.com>
2023-05-25 22:32:26 +0300

    improved showExp function

--- src/Show.hs
+++ src/Show.hs
@@ -3,14 +3,17 @@ module Show where
 import Exp
 import Data.List (intercalate)
 
+inParens :: String -> String
+inParens x = "(" ++ x ++ ")"
+
 showVar :: Var -> String
 showVar (Var x) = x
 
 showExp :: ComplexExp -> String
 showExp (Nat n) = show n
 showExp (CX v) = showVar v
-showExp (CLam v x) = "/" ++ showVar v ++ " -> " ++ showExp x
-showExp (CApp a b) = showExp a ++ "(" ++ showExp b ++ ")"
-showExp (Let v x y) = "l " ++ showVar v ++ " = " ++ showExp x ++ " in " ++ showExp y
-showExp (LetRec v x y) = "lr " ++ showVar v ++ " = " ++ showExp x ++ " in " ++ showExp y
-showExp (List x) = "[" ++ intercalate "," (map showExp x) ++ "]"
+showExp (CLam v x) = inParens ("/" ++ showVar v ++ " -> " ++ showExp x)
+showExp (CApp a b) = inParens (showExp a ++ " " ++ showExp b)
+showExp (Let v x y) = inParens ("l " ++ showVar v ++ " = " ++ showExp x ++ " in " ++ showExp y)
+showExp (LetRec v x y) = inParens ("lr " ++ showVar v ++ " = " ++ showExp x ++ " in " ++ showExp y)
+showExp (List l) = "[" ++ intercalate "," (map showExp l) ++ "]"
