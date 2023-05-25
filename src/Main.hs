4d6144efee3738c034675d5dfe7f0463d031070e
Vlad Ciocoiu <vlad.014916@gmail.com>
2023-05-25 22:31:53 +0300

    Added load file functionality

--- src/Main.hs
+++ src/Main.hs
@@ -6,17 +6,27 @@ import Parsing
 import Show
 import Eval
 import Sugar
+import Program (Environment, normalizeEnv, program, programEnv)
+import Data.Map (empty)
+import Lab2 (parseFromFile)
 
 main :: IO ()
-main = do
+main = execute Data.Map.empty
+    
+execute :: Environment -> IO ()
+execute env = do
     putStr "miniHaskell> "
     hFlush stdout
     s <- getLine
     case testParse replCommand s of
-          Nothing -> putStrLn "Cannot parse command" >> main
+          Nothing -> putStrLn "Cannot parse command" >> execute env
           Just Quit -> return ()
-          Just (Load _) -> putStrLn "Not implemented" >> main
+          Just (Load file) -> do
+                str <- parseFromFile program file
+                case str of
+                    Right s -> let newEnv = programEnv s in execute newEnv
+                    Left err -> putStrLn "Error: cannot read file" >> execute env
           Just (Eval es) ->
             case testParse exprParser es of
-              Nothing -> putStrLn "Error: cannot parse expression" >> main 
-              Just e -> putStrLn (showExp (sugarExp (normalize (desugarExp e)))) >> main
+              Nothing -> putStrLn "Error: cannot parse expression" >> execute env 
+              Just e -> putStrLn (showExp (sugarExp (normalizeEnv env (desugarExp e)))) >> execute env
